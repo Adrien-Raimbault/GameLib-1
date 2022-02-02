fb05d8f01bb7d6a4459565e510e10077787ccaff
Frédéric NOËL <contact@fredericnoel.com>
2022-02-02 09:18:49 +0100

    Test PDO

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 date_default_timezone_set('Europe/Paris');
 setlocale(LC_ALL, '');
 
+spl_autoload_register(function ($className) {
+    include './classes/' . $className . '.php';
+});
+
+$test = new Test;
+
 require_once './functions/autoLoadFunction.php';
 require_once './includes/head.php';
 require_once './includes/main.php';
